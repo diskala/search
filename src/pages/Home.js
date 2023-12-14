408f208f4cbaff5ab7abe787a3bcdd9f87c15945
diskala <143798335+diskala@users.noreply.github.com>
2023-12-14 16:11:16 +0100

    creation les routes +formulaire parcourir  API pays

---
+++ src/pages/Home.js
@@ -0,0 +1,115 @@
+﻿import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+import './Home.css'
+
+const TousLesPays = () => {
+  const [motCle, setMotCle] = useState('');
+  const [resultats, setResultats] = useState([]);
+
+  const formulaire = (e) => {
+    
+    setMotCle(e.target.value);
+  };
+  
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('https://restcountries.com/v3.1/all');
+        
+        if (response.status === 200) {
+          const data = response.data;
+          
+          // Filtrer les pays qui contiennent le mot-clé dans leur nom ou leurs informations
+          const resultatsFiltres = data.filter(
+            (pays) =>
+             
+            pays.translations.fra.common.toLowerCase().includes(motCle.toLowerCase()) 
+             
+           
+          );
+
+          setResultats(resultatsFiltres);
+         
+        } else {
+          console.error(`Erreur lors de la requête à l'API: ${response.status}`);
+        }
+      } catch (error) {
+        console.error('Erreur lors de la requête à l\'API', error);
+      }
+    };
+
+    // Appeler fetchData lorsque le mot-clé change
+    fetchData();
+  }, [motCle]); // Déclencher l'effet lorsque motCle change
+
+  return (
+    <div id="container_tousPays">
+        <form action="" >
+          <fieldset>
+            <legend>Formulaire de Recherche</legend>
+          <input
+        type="text"
+        value={motCle}
+        
+        onChange={formulaire}
+        placeholder="Recherche par mot-clé"
+      />
+        <button type="submit">Search</button>
+
+          </fieldset>
+       
+        </form>
+      
+
+      {/* {resultats.length > 0 && motCle.length > 0(
+        <div>
+          <h2>Résultats de la recherche pour '{motCle}':</h2>
+          <ul>
+            {resultats.map((pays) => (
+                
+              <li key={pays.translations.fra.official}>{pays.translations.fra.official}</li>
+            ))}
+
+
+          </ul>
+        </div>
+      )} */}
+      {motCle.length === null && resultats.length > 0 && (
+        <div>
+          <h2>Résultats de la recherche pour '{motCle}':</h2>
+          <ul>
+            <li></li>
+          </ul>
+        </div>
+      )}
+
+{motCle.length >0 && resultats.length > 0 &&  (
+        <div>
+          <h2>Résultats de la recherche pour '{motCle}':</h2>
+          <ul id="result">
+          {resultats.map((pays) => (
+                
+                <li  className="tousPays" key={pays.translations.fra.common}>{pays.translations.fra.common}
+                 
+                 <Link to= {"/unpays/" + pays.ccn3}> <h1>{pays.translations.fra.common}</h1></Link>
+                <h1>Continent: {pays.region}</h1>
+                <h1>Capitale: {pays.capital}</h1>
+                {pays.flags &&  < Link to= {"/unpays/" + pays.ccn3}><img src={pays.flags.svg} alt={`Drapeau de ${pays.name.common}`} style={{ width: '30rem', height: '15rem' }} /></Link>}
+                
+                </li>
+                
+               
+                      
+              ))}
+    
+          </ul>
+        
+        </div>
+       
+      )}
+    </div>
+  );
+};
+
+export default TousLesPays;
---
+++ src/pages/Pays.js
@@ -0,0 +1,114 @@
+﻿import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import './unpays.css'
+const PaysDetails = () => {
+  const { id } = useParams();
+  const [unpays, setUnpays] = useState(null);
+
+  useEffect(() => {
+    fetch(`https://restcountries.com/v3.1/alpha/${id}`)
+      .then(res => res.json())
+      .then(data => {
+        setUnpays(data);
+      })
+      .catch(error => {
+        console.error('Erreur lors de la requête à l\'API', error);
+      });
+  }, [id]);
+
+  if (!unpays) {
+    return <div>Chargement en cours...</div>;
+  }
+
+  // Vérifier si unpays est un tableau avant d'utiliser .map()
+  if (Array.isArray(unpays)) {
+    return (
+      <div>
+        {unpays.map((pays) => (
+          <div key={pays.ccn3}>
+            <table>
+
+              <th>
+                <td class="th">Pays</td>
+                <td class="th">Nom de la devise</td>
+                <td class="th">Symbole de la devise</td>
+                <td class="th">La langue officiel</td>
+                <td class="th">Pays voisins</td>
+                <td class="th">Lien google maps</td>
+                
+                <tr>
+                  <td>
+                    {pays.translations.fra.common}
+                  </td>
+
+                  <td> 
+                    
+                    {Object.keys(pays.currencies).map((currencyCode) => (
+                      <div key={currencyCode}>
+                            {pays.currencies[currencyCode].name}
+                 
+                  </div>
+                       ))}
+                  </td>
+
+
+                  <td> 
+                    
+                    {Object.keys(pays.currencies).map((currencyCode) => (
+                      <div key={currencyCode}>
+                            {pays.currencies[currencyCode].symbol}
+                 
+                  </div>
+                       ))}
+                  </td>
+
+
+
+                  <td>
+                  {Object.keys(pays.languages).map((currencyCode) => (
+              <div key={currencyCode}>
+                {pays.languages[currencyCode]}
+
+              </div>
+            ))}
+
+                  </td>
+                  <td>
+                  {pays.landlocked === undefined && (
+              <p>{pays.landlocked ? 'Oui' : 'pas de pays voisin'}</p>
+            )}
+            {pays.borders && pays.borders.length > 0 && (
+              <div>
+              
+                <ul>
+                  {pays.borders.map((borderCode) => (
+                    <li key={borderCode}>{borderCode}</li>
+                  ))}
+                </ul>
+              </div>
+            )}
+
+
+                  </td>
+                  <td class="lien">
+
+                  <Link to={pays.maps.googleMaps}><span>Lien google maps</span></Link>
+                  </td>
+                  
+                </tr>
+             
+                </th>
+
+            </table>
+          </div>
+        ))}
+      </div>
+    );
+  } else {
+    // Gérer le cas où unpays n'est pas un tableau
+    return <div>Les données du pays ne sont pas au format attendu.</div>;
+  }
+};
+
+export default PaysDetails;
